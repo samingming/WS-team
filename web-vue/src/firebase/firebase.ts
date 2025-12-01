4c4bd6892c949b29ca60d0e57c42c1476f5ee818
wlsdudwjd <jjyabc11@naver.com>
2025-12-01 21:35:06 +0900

    라우터 설정(로그인 안하면 못들어오게)

--- web-vue/src/firebase/firebase.ts
+++ web-vue/src/firebase/firebase.ts
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
-const auth = getAuth(app)
+export const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 
 export const googleLogin = async () => {
